7159523e10b64db8ab3d65fd46c5667388c9dcaa
IvanZhuravlev <32226412+IvanZhuravlev@users.noreply.github.com>
2019-03-04 20:59:19 +0300

    Update Practice 1.java

--- Practice 1.java
+++ Practice 1.java
@@ -7,13 +7,39 @@ public class Practice {
      double IdealMassMan;
     public static void main(String[] args) {
         Scanner s = new Scanner (System.in);
-        System.out.println("Enter weight");
+        System.out.println("Введите ваш вес");
          double w = s.nextDouble();
-    System.out.println("Enter height");
+    System.out.println("Введите ваш рост");
         double h = s.nextDouble();
         double IdealMassWoman =  h - 110;
         double IdealMassMan =  h - 100;
-        System.out.println("IdealMassWoman="+IdealMassWoman);
-        System.out.print("IdealMassMan="+IdealMassMan); 
+         System.out.println("Введите свой пол");
+         System.out.println("1-Женщина");
+         System.out.println("2-Мужчина");
+        int sex = s.nextInt();
+        switch (sex){
+            case(1):
+        if (w > IdealMassWoman) {
+  System.out.println("Вам необходимо похудеть на "+(w-IdealMassWoman)+"кг");
+        }
+        else if (w<IdealMassWoman) {
+  System.out.println("Вам необходимо поправиться на "+(IdealMassWoman-w)+"кг");
+ }
+        else  if (w == IdealMassWoman){
+     System.out.println("Ваш вес оптимальный");
+ }
+        break;
+        case (2):
+        if (w > IdealMassMan) {
+  System.out.println("Вам необходимо похудеть на "+(w-IdealMassMan)+"кг");
+        }
+        else if (w<IdealMassMan) {
+  System.out.println("Вам необходимо поправиться на "+(IdealMassMan-w)+"кг");
+ }
+        else  if (w == IdealMassMan){
+     System.out.println("Ваш вес оптимальный");
     }
+  break; 
+    }
+}
 }
